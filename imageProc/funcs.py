65ab68379d341ee495d49ecfa9cfcd031b580067
martybillingsley <48107311+martybillingsley@users.noreply.github.com>
2019-04-22 20:35:47 -0500

    Update funcs.py

--- imageProc/funcs.py
+++ imageProc/funcs.py
@@ -1,1 +1,9 @@
 from imageFuncs.py import *
+
+img = makePicture('flower.jpg')
+pixels = img.load()
+for y in range(getHeight(img)):
+    for x in range(getWidth(img)):
+        setRed(img,x,y,0)
+show(img)
+
